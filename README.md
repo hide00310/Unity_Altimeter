318b5fe17742501d5d3abd9569556b2ecd2d9984
hide00310 <111543047+hide00310@users.noreply.github.com>
2023-04-17 10:53:16 +0900

    no message

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Unity_Altimeter
+1. ファイルをダウンロードしてAssetsフォルダに配置
+1. Altimeterシーンを開く
+1. 実行する
+
+Unity : 2019.4.31f1
---
+++ Scripts/Altimeter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Altimeter : MonoBehaviour
+{
+    public float StepPos;
+    public float PosToCanvasUnit;
+    public AltimeterNeedle[] Needles;
+    public Rigidbody CharacterRigidbody;
+
+    private void Update()
+    {
+        float pos = CharacterRigidbody.transform.position.y;
+        for (int i = 0; i < Needles.Length; i++)
+        {
+            float transStep = GetTranslationStep(pos, StepPos, i - Needles.Length / 2);
+            AltimeterNeedle needle = Needles[i];
+            needle.transform.position = transform.position;
+            needle.transform.Translate(0, transStep * PosToCanvasUnit, 0);
+            string num = ((int)Mathf.Abs(pos / StepPos) + i).ToString("00");
+            needle.Texts[0].text = num;
+            needle.Texts[1].text = num;
+        }
+    }
+    private float GetTranslationStep(float pos, float stepPos, int presentStep)
+    {
+        float presentPos = stepPos * (int)(pos / stepPos);
+        float diffPos = presentPos - pos;
+        float transPos = diffPos + stepPos * presentStep;
+        return transPos / stepPos;
+    }
+}
